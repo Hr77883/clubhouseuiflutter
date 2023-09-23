52294753bdeb20a0593110fdfdbf41a1d848e0e0
Hr77883 <145797475+Hr77883@users.noreply.github.com>
2023-09-23 16:35:53 +0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,197 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+void main() => runApp(Calculator());
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _CalculatorState extends State<Calculator> {
+  List content = ["1","2","3","+","4","5","6","-","7","8","9","*","C","0",".","/","AC","="];
+  double result = 0;
+  CustomStk obj = CustomStk();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator"),
+          centerTitle: true, 
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomRight,
+                decoration: BoxDecoration(color: const Color.fromARGB(255, 1, 4, 6)),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    _buildText(result.toString(), 45.0, Colors.white),
+                    _buildText(obj.getExpr(), 30.0, const Color.fromARGB(137, 255, 255, 255)),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              height: 300.0,
+              child: Column(children: List.generate(5, (i) => _buildRows(i))),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+  _buildText(text, size, color) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: size,
+        color: color,
+      ),
+    );
+  }
+  _buildRows(i) {
+    if (i != 4)
+      return Expanded(
+        child: Row(
+            children: List.generate(4, (j) => _buildBtn(j, i * 4))),
+      );
+    else
+      return Expanded(
+        child: Row(children: List.generate(2, (i) => _buildBtn(16, i))),
+      );
+  }
+  _buildBtn(i, count) {
+    String text = content[i + count];
+    return Expanded(
+      child: SizedBox.expand(
+        child: FloatingActionButton(
+          child: _buildText(text, 27.0, Color.fromARGB(255, 255, 255, 255)),
+          onPressed: () {
+            setState(() {
+              if (text == "C") {
+                result = obj.pop();
+              } else if (text == "AC"){
+                obj.clear();
+                result = 0.0;
+              }
+              else if (text == "=") {
+                result = obj.result;
+                obj.clear();
+                obj.cstStk.add(result.toString());
+              } else
+                result = obj.push(text);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+class CustomStk {
+  String num1 = "";
+  double result = 0;
+  int i=0;
+  List oprs = ["+","*","-","/"];
+  List nmbrs = List.generate(11, (n) => (n == 10) ? "." : n.toString());
+  List cstStk = [];
+  List opnStk = [];
+  List oprStk = [];
+  List<double> rstStk = [];
+  
+
+  double push(t) {
+    if (oprs.contains(t)) {
+      (cstStk.isEmpty) ? cstStk.add("0") : 0;
+      cstStk.add(t);
+      num1 = "";
+    } else if (nmbrs.contains(t)) {
+      num1 += t;
+      if (cstStk.isNotEmpty && !oprs.contains(cstStk.last)) cstStk.removeLast();
+      cstStk.add(num1);
+    }
+    result = recalc();
+    return result;
+  }
+  double recalc() {
+    String t = "";
+    oprStk.clear();
+    opnStk.clear();
+    rstStk.clear();
+    for (i = 0; i < cstStk.length; i++) {
+      t = cstStk[i];
+      if (oprs.contains(t)) {
+        while (oprStk.isNotEmpty && highP(t, oprStk.last)) {
+          opnStk.add(oprStk.last);
+          oprStk.removeLast();
+        }
+        oprStk.add(t);
+      } else
+        opnStk.add(t);
+    }
+    while (oprStk.isNotEmpty) {
+      opnStk.add(oprStk.last);
+      oprStk.removeLast();
+    }
+    for (i = 0; i < opnStk.length; i++) {
+      t = opnStk[i];
+      if (oprs.contains(t)) doOp(t);
+      else rstStk.add(double.parse(t));
+    }
+    if (rstStk.isEmpty) return 0;
+    else return rstStk[0];
+  }
+  bool highP(t, topOfStk) {
+    int currentP = getP(t);
+    int tosP = getP(topOfStk);
+    if (t == topOfStk) return false;
+    if (tosP >= currentP) return true;
+    else return false;
+  }
+  int getP(t) {
+    if (t == "*" || t == "/") return 2;
+    else return 1;
+  }
+  doOp(symbol) {
+    double num1, num2;
+    if (rstStk.length == 1) return rstStk[0];
+    num1 = rstStk.last;
+    rstStk.removeLast();
+    num2 = rstStk.last;
+    rstStk.removeLast();
+    if (symbol == "+") rstStk.add(num1 + num2);
+    else if (symbol =="-") rstStk.add(num2 - num1);
+    else if (symbol == "*") rstStk.add(num1 * num2);
+    else rstStk.add(num2 / num1);
+  }
+  pop() {
+    num1 = "";
+    if (cstStk.isNotEmpty) {
+      String tmp = cstStk.last;
+      if (tmp.length == 1) cstStk.removeLast();
+      else {
+        tmp = tmp.substring(0, tmp.length - 1);
+        cstStk.removeLast();
+        cstStk.add(tmp);
+      }
+    }
+    return recalc();
+  }
+  clear() {
+    if(rstStk.isNotEmpty) result = rstStk[0];
+    num1 = "";
+    cstStk.clear();
+    opnStk.clear();
+    oprStk.clear();
+    rstStk.clear();
+    return result;
+  }
+  getExpr() {
+    String temp = "";
+    for (i = 0; i < cstStk.length; i++) temp += cstStk[i];
+    return temp;
   }
 }
